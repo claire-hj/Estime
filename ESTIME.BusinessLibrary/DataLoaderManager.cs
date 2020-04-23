8a533bf7726297ce60f5f38d1612e843cd2f86de
yenhi75 <63600958+yenhi75@users.noreply.github.com>
2020-04-23 12:28:53 -0400

    Texloader code

--- ESTIME.BusinessLibrary/DataLoaderManager.cs
+++ ESTIME.BusinessLibrary/DataLoaderManager.cs
@@ -27,7 +27,7 @@ namespace ESTIME.BusinessLibrary
         ExcelPackage ws;
         private bool loadSuccess;
         private string loadErr = string.Empty;
-
+        private List<TdLoadStaging> newStagings = new List<TdLoadStaging>();
         public DataLoaderManager(IConfiguration config)
             : 
             base(config)
@@ -66,13 +66,16 @@ namespace ESTIME.BusinessLibrary
                 //estimeFileType = dal.GetEstimeFileTypeId("3");
                 if (estimeFileType.FileType.Extension == ".txt" || estimeFileType.FileType.Extension == ".csv")
                 {
-                    //loading text file
-                    loadSuccess = dal.LoadTextDataFileByBulk(curLoad.Id, refPeriodId);
+                    //Read loading text file into tdLoadStaging object
+                    loadSuccess = ReadTextFile();
 
-                    //new code to construct the loadStagings list from the text file
-                    //To test, comment the call above and uncomment the code below
-                    List<TdLoadStaging> loadStagings = new List<TdLoadStaging>();
-                    loadSuccess = dal.AddTdLoadStaging(curLoad.Id, refPeriodId, loadStagings);
+                    //save tdLoadStaging object to staging table
+
+                    if (loadSuccess)
+                    {
+                        dal.AddTdLoadStaging(curLoad.Id, refPeriodId,newStagings);
+
+                    }
                 }
                 else if (estimeFileType.FileType.Extension == ".xlsx")
                 {
@@ -185,7 +188,34 @@ namespace ESTIME.BusinessLibrary
             }
         }
 
+        public bool ReadTextFile()
+        {
 
+            String FileName = curLoad.FilePath + estimeFileType.FileType.Extension;
+            using (StreamReader file = new StreamReader(FileName))
+            {
+                //file = new StreamReader(curLoad.FilePath + estimeFileType.FileType.Extension);
+
+                int LineNumber = 0;
+                string ln;
+
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    TdLoadStaging newLoad = new TdLoadStaging(curLoad.Id, +LineNumber, ln);
+
+                    newStagings.Add(newLoad);
+                }
+                file.Close();
+                if (LineNumber == 0)
+                {
+                    loadErr = FileName + " is empty!";
+                    return false;
+                }
+
+            }
+            return true;
+        }
 
     }
 }
